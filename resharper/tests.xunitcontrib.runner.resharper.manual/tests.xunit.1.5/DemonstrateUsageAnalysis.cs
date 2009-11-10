75062407ae9a06c7050b3a4bc0d96d81a13f9b76
citizenmatt <m.t.ellis@gmail.com>
2009-11-10 23:16:56 +0000

    Updated usage tests

--- resharper/tests.xunitcontrib.runner.resharper.manual/tests.xunit.1.5/DemonstrateUsageAnalysis.cs
+++ resharper/tests.xunitcontrib.runner.resharper.manual/tests.xunit.1.5/DemonstrateUsageAnalysis.cs
@@ -6,86 +6,89 @@ using Xunit.Extensions;
 
 namespace tests.xunit
 {
-    // TEST: This namespace should be marked as in use
-    namespace NamespaceContainingSingleClassMarkedAsUsed
+    namespace DemonstrateUsageAnalysis
     {
+        // TEST: This namespace should be marked as in use
+        namespace NamespaceContainingSingleClassMarkedAsUsed
+        {
+            // TEST: This class should be marked as in use
+            public class SingleTestClass
+            {
+                // TEST: This method should be marked as in use
+                [Fact]
+                public void TestMethodMarkedAsInUse()
+                {
+                    Assert.Equal(1, 1);
+                }
+            }
+        }
+
+        // For completeness...
+        namespace EmptyNamespaceMarkedAsNotInUse
+        {
+        }
+
+        // TEST: Parent class should be marked as in use
+        public class ParentClassMarkedAsInUse
+        {
+            // TEST: Nested class should be marked as in use
+            // TEST: This class should get a test class marker
+            public class NestedClassMarkedAsInUse
+            {
+                [Fact]
+                public void TestMethodMarkedAsInUse()
+                {
+                    Assert.Equal(1, 1);
+                }
+            }
+        }
+
         // TEST: This class should be marked as in use
-        public class SingleTestClass
+        // TEST: This class should be flagged as a test class
+        public class ClassInUse
         {
             // TEST: This method should be marked as in use
+            // TEST: This method should *NOT* be flagged as a test
             [Fact]
-            public void TestMethodMarkedAsInUse()
+            public void TestMethodMarkedInUse()
             {
-                Assert.Equal(1, 1);
             }
         }
-    }
 
-    // For completeness...
-    namespace EmptyNamespaceMarkedAsNotInUse
-    {
-    }
+        // TEST: This class should be marked as in use
+        public class BaseClassFixtureMarkedAsInUse
+        {
+        }
 
-    // TEST: Parent class should be marked as in use
-    public class ParentClassMarkedAsInUse
-    {
-        // TEST: Nested class should be marked as in use
-        // TEST: This class should get a test class marker
-        public class NestedClassMarkedAsInUse
+        // TEST: This class should be marked as in use
+        public class DerivedClassMarkedAsInUse : BaseClassFixtureMarkedAsInUse
         {
             [Fact]
-            public void TestMethodMarkedAsInUse()
+            public void TestMethod()
             {
-                Assert.Equal(1, 1);
             }
         }
-    }
 
-    // TEST: This class should be marked as in use
-    // TEST: This class should be flagged as a test class
-    public class ClassInUse
-    {
-        // TEST: This method should be marked as in use
-        // TEST: This method should *NOT* be flagged as a test
-        [Fact]
-        public void TestMethodMarkedInUse()
+        public class PropertyDataTheoryTest
         {
-        }
-    }
+            [Theory]
+            [PropertyData("TheoryDataEnumerator")]
+            public void DataFromProperty(int value)
+            {
+                Console.WriteLine("DataFromProperty({0})", value);
+            }
 
-    // TEST: This class should be marked as in use
-    public class BaseClassFixtureMarkedAsInUse
-    {
-    }
+            // TEST: This property should be marked as in use
+            public static IEnumerable<object[]> TheoryDataEnumerator
+            {
+                get { return Enumerable.Range(1, 10).Select(x => new object[] { x }); }
+            }
 
-    // TEST: This class should be marked as in use
-    public class DerivedClassMarkedAsInUse : BaseClassFixtureMarkedAsInUse
-    {
-        [Fact]
-        public void TestMethod()
-        {
-        }
-    }
-
-    public class PropertyDataTheoryTest
-    {
-        [Theory]
-        [PropertyData("TheoryDataEnumerator")]
-        public void DataFromProperty(int value)
-        {
-            Console.WriteLine("DataFromProperty({0})", value);
-        }
-
-        // TEST: This property should be marked as in use
-        public static IEnumerable<object[]> TheoryDataEnumerator
-        {
-            get { return Enumerable.Range(1, 10).Select(x => new object[] { x }); }
-        }
-
-        // TEST: This propert should be marked as *NOT* in use
-        public static IEnumerable<object[]> NotTheoryDataEnumerator
-        {
-            get { return null; }
+            // TEST: This propert should be marked as *NOT* in use
+            public static IEnumerable<object[]> NotTheoryDataEnumerator
+            {
+                get { return null; }
+            }
         }
     }
 }

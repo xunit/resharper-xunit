7967c10e09cb05cf3ea788371f9237e291d14805
citizenmatt <m.t.ellis@gmail.com>
2011-05-06 20:51:06 +0100

    Updated the manual tests for the external annotations to be a little more self explanatory

--- resharper/tests.xunitcontrib.runner.resharper.manual/tests.xunit.1.1/ExternalAnnotations/VerifyExternalAnnotations.cs
+++ resharper/tests.xunitcontrib.runner.resharper.manual/tests.xunit.1.1/ExternalAnnotations/VerifyExternalAnnotations.cs
@@ -13,10 +13,15 @@ namespace tests.xunit.ExternalAnnotations
         {
             try
             {
-                AssertIsNotNullRemovesNullReferenceWarning();
-                AssertIsNullProbablyDoesSomethingUsefulButIDontKnowWhat();
-                AssertIsTruePreventsAlwaysFalseExpressionBeingFlagged();
-                AssertIsFalsePreventsAlwaysTrueExpressionBeingFlagged();
+                AssertIsNotNull_RemovesNullReferenceWarning_PassingVariableToNotNullMethod();
+                AssertIsNotNull_RemovesNullReferenceWarning_AccessingPropertiesOnVariable();
+                AssertIsNotNull_RemovesNullReferenceWarning_FromValueFlaggedWithCanBeNull();
+
+                AssertIsNull_AddsNullReferenceWarning();
+
+                AssertIsTrue_AddsExpressionAlwaysTrueWarning();
+
+                AssertIsFalse_AddsExpressionAlwaysFalseWarning();
             }
             catch
             {
@@ -28,59 +33,127 @@ namespace tests.xunit.ExternalAnnotations
         // If we want to try one of their marked classes, try System.Activator.CreateInstance((Type)null)
         private static void MethodWithNotNullParameter([NotNull] string stringCannotBeNull)
         {
-            stringCannotBeNull.ToLower();
+            Console.WriteLine(stringCannotBeNull.ToLower());
         }
 
-        private static void AssertIsNotNullRemovesNullReferenceWarning()
+        private static void MethodWithoutNotNullParameter(string stringValue)
+        {
+            Console.WriteLine(stringValue.ToLower());
+        }
+
+        [CanBeNull]
+        private static string MethodWithCanBeNullReturnValue()
+        {
+            return null;
+        }
+
+        private static void AssertIsNotNull_RemovesNullReferenceWarning_PassingVariableToNotNullMethod()
         {
             string nullStringValue = null;
+            string uncheckedNullStringValue = null;
 
             Assert.NotNull(nullStringValue);
 
-            // TEST: nullStringValue is not flagged as "Possible System.NullReferenceException"
+            // At this point, resharper knows that nullStringValue is NOT null (we'll have thrown an exception if it IS null)
+            // IsNotNull doesn't have a [NotNull] attribute because IsNotNull would have a "Check for null" warning
+            // which is kind of redundant for an assert
+
+            // TEST: nullStringValue is not flagged
             MethodWithNotNullParameter(nullStringValue);
+
+            // CONTROL: uncheckedNullStringValue should be flagged as "Expression is always null"
+            MethodWithoutNotNullParameter(uncheckedNullStringValue);
         }
 
-        private static void AssertIsNullProbablyDoesSomethingUsefulButIDontKnowWhat()
+        private static void AssertIsNotNull_RemovesNullReferenceWarning_AccessingPropertiesOnVariable()
         {
-            object o = new FlagsAttribute();
-            var o2 = o as AttributeUsageAttribute;
+            string nullStringValue = null;
+            string uncheckedNullStringValue = null;
 
-            Assert.Null(o2);
+            Assert.NotNull(nullStringValue);
 
-            // TEST: I can't figure out a really sensible test for Assert.IsNull, but this seems to do 
-            // something. If Assert.IsNull has the attribute, o2 == null below *IS* flagged as "Expression
-            // is always true"
-            if (o2 == null)
-                return;
+            // At this point, resharper knows that nullStringValue is NOT null (we'll have thrown an exception if it IS null)
+            // IsNotNull doesn't have a [NotNull] attribute because IsNotNull would have a "Check for null" warning
+            // which is kind of redundant for an assert
+
+            // TEST: nullStringValue is not flagged
+            Console.WriteLine(nullStringValue.Length);
+
+            // CONTROL: uncheckedNullStringValue should be flagged as "Possible System.NullReferenceException"
+            Console.WriteLine(uncheckedNullStringValue.Length);
         }
 
-        private static void AssertIsTruePreventsAlwaysFalseExpressionBeingFlagged()
+        private static void AssertIsNotNull_RemovesNullReferenceWarning_FromValueFlaggedWithCanBeNull()
         {
-            const bool myFlag = false;
+            var canBeNullValue = MethodWithCanBeNullReturnValue();
+            var uncheckedCanBeNullValue = MethodWithCanBeNullReturnValue();
 
-            Assert.True(myFlag);
+            Assert.NotNull(canBeNullValue);
 
-            // TEST: This condition should not be flagged as "Expression is always true"
-            // TEST: RS 5.0 flags this as heuristically unreachable
-            while (!myFlag)
-            {
-                break;
-            }
+            // At this point, resharper knows that nullStringValue is NOT null (we'll have thrown an exception if it IS null)
+            // IsNotNull doesn't have a [NotNull] attribute because IsNotNull would have a "Check for null" warning
+            // which is kind of redundant for an assert
+
+            // TEST: canBeNullValue should NOT be flagged
+            Console.WriteLine(canBeNullValue.Length);
+
+            // CONTROL: uncheckedCanBeNullValue should be flagged as "Possible System.NullReferenceException"
+            Console.WriteLine(uncheckedCanBeNullValue.Length);
         }
 
-        private static void AssertIsFalsePreventsAlwaysTrueExpressionBeingFlagged()
+        private static void AssertIsNull_AddsNullReferenceWarning()
         {
-            const bool myFlag = true;
+            var canBeNullValue = MethodWithCanBeNullReturnValue();
+            var uncheckedCanBeNullValue = MethodWithCanBeNullReturnValue();
 
-            Assert.False(myFlag);
+            Assert.Null(canBeNullValue);
 
-            // TEST: This condition should not be flagged as "Expression is always true"
-            // TEST: RS 5.0 flags this as heuristically unreachable
-            while (myFlag)
-            {
-                break;
-            }
+            // At this point, we've told resharper that canBeNullValue IS null (we'll have thrown an exception if it's NOT)
+
+            // TEST: (canBeNullValue == null) is flagged as "Expression is always true"
+            if (canBeNullValue == null)
+                Console.WriteLine("Is null");
+
+            // CONTROL: (uncheckedCanBeNullValue == null) is not flagged
+            if (uncheckedCanBeNullValue == null)
+                Console.WriteLine("Is null");
+        }
+
+        private static bool MethodReturnsBool(bool value)
+        {
+            return value;
+        }
+
+        private static void AssertIsTrue_AddsExpressionAlwaysTrueWarning()
+        {
+            var trueValue = MethodReturnsBool(true);
+            var uncheckedTrueValue = MethodReturnsBool(true);
+
+            Assert.True(trueValue);
+
+            // At this point, resharper knows that trueValue is always true
+
+            // TEST: trueValue is flagged as "Expression is always true"
+            Console.WriteLine(trueValue ? "true" : "false");
+
+            // CONTROL: uncheckedTrueValue is NOT flagged
+            Console.WriteLine(uncheckedTrueValue ? "true" : "false");
+        }
+
+        private static void AssertIsFalse_AddsExpressionAlwaysFalseWarning()
+        {
+            var falseValue = MethodReturnsBool(false);
+            var uncheckedFalseValue = MethodReturnsBool(false);
+
+            Assert.False(falseValue);
+
+            // At this point, resharper knows that falseValue is always false
+
+            // TEST: trueValue is flagged as "Expression is always false"
+            Console.WriteLine(falseValue ? "true" : "false");
+
+            // CONTROL: uncheckedTrueValue is NOT flagged
+            Console.WriteLine(uncheckedFalseValue ? "true" : "false");
         }
     }
 }

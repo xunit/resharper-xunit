50f46af5bb0e640a5251bfb2a628837e15beeb81
citizenmatt <m.t.ellis@gmail.com>
2011-08-11 22:59:46 +0100

    Added manual tests for xunit + dotCover. They currently fail with resharper 6 (haven't tested in earlier versions)

--- resharper/tests.xunitcontrib.runner.resharper.manual/tests.xunit.1.6.1/CodeCoverage/CodeCoverageTests.cs
+++ resharper/tests.xunitcontrib.runner.resharper.manual/tests.xunit.1.6.1/CodeCoverage/CodeCoverageTests.cs
@@ -1,48 +1,47 @@
-using NUnit.Framework;
+using Xunit;
 
-namespace tests.reference.nunit.CodeCoverage
+namespace tests.xunit.CodeCoverage
 {
     // Testing integration with dotCover
-    [TestFixture]
     public class CodeCoverageTests
     {
-        [Test]
+        [Fact]
         public void CoverConstructor()
         {
             var calculator = new Calculator(23);
-            Assert.IsNotNull(calculator);
+            Assert.NotNull(calculator);
         }
 
-        [Test]
+        [Fact]
         public void CoverAddition()
         {
             var calculator = new Calculator(23);
             var result = calculator.Add(2);
-            Assert.AreEqual(25, result);
+            Assert.Equal(25, result);
         }
 
-        [Test]
+        [Fact]
         public void CoverSubtraction()
         {
             var calculator = new Calculator(33);
             var result = calculator.Subtract(5);
-            Assert.AreEqual(28, result);
+            Assert.Equal(28, result);
         }
 
-        [Test]
+        [Fact]
         public void CoverMultiplication()
         {
             var calculator = new Calculator(53);
             var result = calculator.Multiply(2);
-            Assert.AreEqual(106, result);
+            Assert.Equal(106, result);
         }
 
-        [Test, Ignore("Ignoring so that Calculator.Divide is not covered")]
+        [Fact(Skip = "Ignoring so that Calculator.Divide is not covered")]
         public void CoverDivision()
         {
             var calculator = new Calculator(88);
             var result = calculator.Divide(2);
-            Assert.AreEqual(44, result);
+            Assert.Equal(44, result);
         }
     }
 }

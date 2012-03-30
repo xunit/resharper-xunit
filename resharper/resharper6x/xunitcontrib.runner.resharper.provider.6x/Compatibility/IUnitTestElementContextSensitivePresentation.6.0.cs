d35876156741ffb2a574f37f5ebe802c0cad39a9
citizenmatt <m.t.ellis@gmail.com>
2012-03-30 20:49:07 +0100

    Removed support for ReSharper 6.0. Download the free update to 6.1
    Use the xunitcontrib-resharper.0.5.2 tag for the last release with 6.0 support

--- resharper/resharper6x/xunitcontrib.runner.resharper.provider.6x/Compatibility/IUnitTestElementContextSensitivePresentation.6.0.cs
+++
@@ -1,8 +0,0 @@
-namespace JetBrains.ReSharper.UnitTestFramework
-{
-    // Not implemented by 6.0
-    public interface IUnitTestElementContextSensitivePresentation
-    {
-        string GetPresentation(IUnitTestElement parentElement);
-    }
-}

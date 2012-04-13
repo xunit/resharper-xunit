d305ea44c97fb105c4b3cd3bbe666b14ced6092b
citizenmatt <m.t.ellis@gmail.com>
2012-04-13 23:55:26 +0100

    The EAP version numbers are now part of the assembly info

--- resharper/resharper6x/ProductInfo.dotCover.2.0.cs
+++ resharper/resharper6x/ProductInfo.dotCover.2.0.cs
@@ -2,5 +2,5 @@ public static class ProductInfo
 {
     public const string Product = "dotCover";
     public const string AssemblyProduct = "dotcover";
-    public const string Version = "2.0";
+    public const string Version = "2.0.406";
 }

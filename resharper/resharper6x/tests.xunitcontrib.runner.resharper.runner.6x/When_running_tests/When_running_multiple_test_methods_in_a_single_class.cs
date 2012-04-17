d7006afb66ad35ae1f9158d864ba3fed0f3780b1
citizenmatt <m.t.ellis@gmail.com>
2012-04-17 08:06:51 +0100

    Added tests for running multiple test methods in a single class

---
+++ resharper/resharper6x/tests.xunitcontrib.runner.resharper.runner.6x/When_running_tests/When_running_multiple_test_methods_in_a_single_class.cs
@@ -0,0 +1,86 @@
+using JetBrains.ReSharper.TaskRunnerFramework;
+using Xunit;
+using Xunit.Sdk;
+
+namespace XunitContrib.Runner.ReSharper.RemoteRunner.Tests.When_running_tests
+{
+    public class When_running_multiple_test_methods_in_a_single_class
+    {
+        private readonly TestClassRun testClass;
+        private readonly FakeRemoteTaskServer taskServer;
+
+        public When_running_multiple_test_methods_in_a_single_class()
+        {
+            testClass = new TestClassRun("TestsNamespace.TestClass");
+            taskServer = new FakeRemoteTaskServer();
+        }
+
+        private ReSharperRunnerLogger CreateLogger()
+        {
+            return new ReSharperRunnerLogger(taskServer, testClass.ClassTask, testClass.MethodTasks);
+        }
+
+        [Fact]
+        public void Should_notify_test_starting_for_each_test_method()
+        {
+            var method1 = testClass.AddPassingTest("TestMethod1");
+            var method2 = testClass.AddPassingTest("TestMethod2");
+            var logger = CreateLogger();
+            testClass.Run(logger);
+
+            taskServer.Messages.AssertContainsTaskStarting(method1.Task);
+            taskServer.Messages.AssertContainsTaskStarting(method2.Task);
+        }
+
+        [Fact]
+        public void Should_notify_test_finished_for_each_test_method()
+        {
+            var method1 = testClass.AddPassingTest("TestMethod1");
+            var method2 = testClass.AddPassingTest("TestMethod2");
+            var logger = CreateLogger();
+            testClass.Run(logger);
+
+            taskServer.Messages.AssertContainsTaskFinished(method1.Task, string.Empty, TaskResult.Success);
+            taskServer.Messages.AssertContainsTaskFinished(method2.Task, string.Empty, TaskResult.Success);
+        }
+
+        [Fact]
+        public void Should_notify_output_for_each_test_method()
+        {
+            const string expectedOutput1 = "This is some output";
+            const string expectedOutput2 = "This is some output for method2";
+            var method1 = testClass.AddPassingTest("TestMethod1", expectedOutput1);
+            var method2 = testClass.AddPassingTest("TestMethod2", expectedOutput2);
+            var logger = CreateLogger();
+            testClass.Run(logger);
+
+            taskServer.Messages.AssertContainsTaskOutput(method1.Task, expectedOutput1);
+            taskServer.Messages.AssertContainsTaskOutput(method2.Task, expectedOutput2);
+        }
+
+        [Fact]
+        public void Should_notify_test_finished_before_starting_next_test()
+        {
+            var method1 = testClass.AddPassingTest("TestMethod1");
+            var method2 = testClass.AddPassingTest("TestMethod2");
+            var logger = CreateLogger();
+            testClass.Run(logger);
+
+            var messages = taskServer.Messages.AssertContainsTaskStarting(method1.Task);
+            messages.AssertContainsTaskFinished(method2.Task, string.Empty, TaskResult.Success);
+        }
+
+        [Fact]
+        public void Should_continue_running_tests_after_failing_test_method()
+        {
+            var exception = new SingleException(33);
+            var method1 = testClass.AddFailingTest("TestMethod1", exception);
+            var method2 = testClass.AddPassingTest("TestMethod2");
+            var logger = CreateLogger();
+            testClass.Run(logger);
+
+            var messages = taskServer.Messages.AssertContainsTaskException(method1.Task, exception);
+            messages.AssertContainsTaskStarting(method2.Task);
+        }
+    }
+}

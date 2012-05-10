0dd1e7956f72aebf3f2e6e2ea34aa6fbcaa68c6d
citizenmatt <m.t.ellis@gmail.com>
2012-05-10 01:17:43 +0100

    Tests for fixture failing in dispose. Disappointingly, these are almost identical to the tests for failing in constructor. Need a better way of setting up these tests

--- resharper/resharper6x/tests.xunitcontrib.runner.resharper.runner.6x/When_running_tests/When_class_initialisation_fails.cs
+++ resharper/resharper6x/tests.xunitcontrib.runner.resharper.runner.6x/When_running_tests/When_class_initialisation_fails.cs
@@ -1,11 +1,5 @@
 namespace XunitContrib.Runner.ReSharper.RemoteRunner.Tests.When_running_tests
 {
-    public class When_class_fixture_cleanup_fails
-    {
-        // An exception is thrown in a test class' fixture object's dispose method
-        // IRunnerLogger.ClassFailed
-    }
-
     public class When_a_catastrophic_error_occurs
     {
         // An exception is thrown in the xunit infrastructure. Not related to test failures

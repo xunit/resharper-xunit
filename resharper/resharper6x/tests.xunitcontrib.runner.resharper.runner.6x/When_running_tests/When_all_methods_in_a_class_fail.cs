acc61d704c96de64630623ef97d18b5f4a70f356
citizenmatt <m.t.ellis@gmail.com>
2012-05-10 01:40:47 +0100

    Tests for when all methods in a class fail

---
+++ resharper/resharper6x/tests.xunitcontrib.runner.resharper.runner.6x/When_running_tests/When_all_methods_in_a_class_fail.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace XunitContrib.Runner.ReSharper.RemoteRunner.Tests.When_running_tests
+{
+    public class When_all_methods_in_a_class_fail : SingleClassTestRunContext
+    {
+        [Fact]
+        public void Should_notify_class_as_completed_successfully()
+        {
+            testClass.AddFailingTest("TestMethod1", new InvalidOperationException("Broken1"));
+            testClass.AddFailingTest("TestMethod2", new InvalidOperationException("Broken2"));
+
+            Run();
+
+            Messages.AssertContainsSuccessfulTaskFinished(testClass.ClassTask);
+        }
+    }
+}

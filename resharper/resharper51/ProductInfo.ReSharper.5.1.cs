fe7f038065cfe4018d37e13d5f65dd51c4a788af
citizenmatt <m.t.ellis@gmail.com>
2012-07-31 11:09:34 +0100

    Removed resharper 5.1 source code. Now only supporting 6.1 and 7.0

--- resharper/resharper51/ProductInfo.ReSharper.5.1.cs
+++
@@ -1,6 +0,0 @@
-public class ProductInfo
-{
-    public const string Product = "ReSharper";
-    public const string AssemblyProduct = "resharper";
-    public const string Version = "5.1";
-}
--- resharper/resharper51/xunitcontrib.runner.resharper.provider.5.1/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-using JetBrains.UI.Application.PluginSupport;
-
-[assembly : AssemblyTitle("xUnit.net unit test provider for ReSharper 5.1")]
-
-[assembly : PluginTitle("xUnit.net unit test provider for ReSharper 5.1")]
-[assembly : PluginDescription("Allows ReSharper 5.1 to run unit tests from xUnit.net")]
-[assembly : PluginVendor("xunitcontrib Team")]
-

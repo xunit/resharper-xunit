09f6220ec165ad70f14775d58c4de815619ef09c
citizenmatt <m.t.ellis@gmail.com>
2012-11-14 11:36:45 +0000

    Removed support for ReSharper 7.0
    ReSharper 7.1 is a free upgrade. Download is available in the archived versions
    (http://xunitcontrib.codeplex.com/releases/view/77716). See the
    xunitcontrib-resharper.0.6.1 tag for the source of the last release

--- 3rdParty/Get3rdParty.bat
+++ 3rdParty/Get3rdParty.bat
@@ -5,7 +5,7 @@ if "%ProgFiles%"=="" SET ProgFiles=%ProgramFiles%
 
 echo %ProgFiles%
 
-if not exist "%ProgFiles%\JetBrains\Resharper\v6.1\bin" goto CopyResharper_v70
+if not exist "%ProgFiles%\JetBrains\Resharper\v6.1\bin" goto CopyResharper_v71
 
 mkdir ReSharper_v6.1
 cd ReSharper_v6.1
@@ -30,35 +30,6 @@ copy "%ProgFiles%\JetBrains\ReSharper\v6.1\Bin\JetBrains.ReSharper.UnitTestExplo
 copy "%ProgFiles%\JetBrains\ReSharper\v6.1\Bin\JetBrains.ReSharper.UnitTestFramework.???" > nul
 cd ..
 echo Support for ReSharper 6.1 successfully copied.
-
-:CopyResharper_v70
-
-if not exist "%ProgFiles%\JetBrains\Resharper\v7.0\bin" goto CopyResharper_v71
-
-mkdir ReSharper_v7.0
-cd ReSharper_v7.0
-
-copy "%ProgFiles%\JetBrains\ReSharper\v7.0\Bin\JetBrains.Annotations.???" > nul
-copy "%ProgFiles%\JetBrains\ReSharper\v7.0\Bin\JetBrains.Platform.ReSharper.ComponentModel.???" > nul
-copy "%ProgFiles%\JetBrains\ReSharper\v7.0\Bin\JetBrains.Platform.ReSharper.DocumentManager.???" > nul
-copy "%ProgFiles%\JetBrains\ReSharper\v7.0\Bin\JetBrains.Platform.ReSharper.DocumentModel.???" > nul
-copy "%ProgFiles%\JetBrains\ReSharper\v7.0\Bin\JetBrains.Platform.ReSharper.IDE.???" > nul
-copy "%ProgFiles%\JetBrains\ReSharper\v7.0\Bin\JetBrains.Platform.ReSharper.MetaData.???" > nul
-copy "%ProgFiles%\JetBrains\ReSharper\v7.0\Bin\JetBrains.Platform.ReSharper.ProjectModel.???" > nul
-copy "%ProgFiles%\JetBrains\ReSharper\v7.0\Bin\JetBrains.Platform.ReSharper.Shell.???" > nul
-copy "%ProgFiles%\JetBrains\ReSharper\v7.0\Bin\JetBrains.Platform.Resharper.UI.???" > nul
-copy "%ProgFiles%\JetBrains\ReSharper\v7.0\Bin\JetBrains.Platform.ReSharper.Util.???" > nul
-copy "%ProgFiles%\JetBrains\ReSharper\v7.0\Bin\JetBrains.ReSharper.Daemon.???" > nul
-copy "%ProgFiles%\JetBrains\ReSharper\v7.0\Bin\JetBrains.ReSharper.Daemon.Engine.???" > nul
-copy "%ProgFiles%\JetBrains\ReSharper\v7.0\Bin\JetBrains.ReSharper.Features.Common.???" > nul
-copy "%ProgFiles%\JetBrains\ReSharper\v7.0\Bin\JetBrains.ReSharper.Feature.Services.???" > nul
-copy "%ProgFiles%\JetBrains\ReSharper\v7.0\Bin\JetBrains.ReSharper.Psi.???" > nul
-copy "%ProgFiles%\JetBrains\ReSharper\v7.0\Bin\JetBrains.ReSharper.Resources.???" > nul
-copy "%ProgFiles%\JetBrains\ReSharper\v7.0\Bin\JetBrains.ReSharper.TaskRunnerFramework.???" > nul
-copy "%ProgFiles%\JetBrains\ReSharper\v7.0\Bin\JetBrains.ReSharper.UnitTestExplorer.???" > nul
-copy "%ProgFiles%\JetBrains\ReSharper\v7.0\Bin\JetBrains.ReSharper.UnitTestFramework.???" > nul
-cd ..
-echo Support for ReSharper 7.0 successfully copied.
 
 :CopyResharper_v71
 

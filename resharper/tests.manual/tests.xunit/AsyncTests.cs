0dc9bab9e93bfff86806652347bd64a528e2c0e6
citizenmatt <m.t.ellis@gmail.com>
2013-10-13 10:35:47 +0100

    Added simple async test

---
+++ resharper/tests.manual/tests.xunit/AsyncTests.cs
@@ -0,0 +1,26 @@
+using System.Threading.Tasks;
+using Xunit;
+
+namespace tests.xunit.passing
+{
+    public class AsyncTests
+    {
+        async static Task<int> Fibonacci(int n)
+        {
+            if (n == 0) { return 0; }
+            if (n == 1) { return 1; }
+
+            // run one of the recursions concurrently. This runs on a background thread
+            var t1 = Task.Factory.StartNew(() => Fibonacci(n - 1));
+            var t2 = Fibonacci(n - 2);
+            return (await (await t1)) + (await t2);
+        }
+
+        [Fact]
+        public async Task SimpleAsyncTest()
+        {
+            var result = await Fibonacci(10);
+            Assert.Equal(55, result);
+        }
+    }
+}

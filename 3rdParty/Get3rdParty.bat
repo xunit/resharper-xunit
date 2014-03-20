3b155d97004b14cbc56aa296baa92a91797f2797
citizenmatt <m.t.ellis@gmail.com>
2014-03-20 09:38:50 +0000

    Removed support for dotCover 2.0 - 2.5. Download binaries on codeplex

--- 3rdParty/Get3rdParty.bat
+++ 3rdParty/Get3rdParty.bat
@@ -40,7 +40,7 @@ echo Support for ReSharper 6.1 successfully copied.
 
 :CopyResharper_v71
 
-if not exist "%ProgFiles%\JetBrains\Resharper\v7.1\bin" goto CopyDotCover_v20
+if not exist "%ProgFiles%\JetBrains\Resharper\v7.1\bin" goto CopyDotCover_v26
 
 mkdir ReSharper_v7.1
 cd ReSharper_v7.1
@@ -71,107 +71,6 @@ copy "%ProgFiles%\JetBrains\ReSharper\v7.1\Bin\JetBrains.ReSharper.UnitTestExplo
 copy "%ProgFiles%\JetBrains\ReSharper\v7.1\Bin\JetBrains.ReSharper.UnitTestFramework.???" > nul
 cd ..
 echo Support for ReSharper 7.1 successfully copied.
-
-:CopyDotCover_v20
-
-if not exist "%ProgFiles%\JetBrains\dotCover\v2.0\bin" goto CopyDotCover_v21
-
-mkdir dotCover_v2.0
-cd dotCover_v2.0
-
-copy "%ProgFiles%\JetBrains\dotCover\v2.0\Bin\JetBrains.Annotations.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.0\Bin\JetBrains.Platform.dotCover.ComponentModel.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.0\Bin\JetBrains.Platform.dotCover.DocumentModel.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.0\Bin\JetBrains.Platform.dotCover.IDE.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.0\Bin\JetBrains.Platform.dotCover.MetaData.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.0\Bin\JetBrains.Platform.dotCover.ProjectModel.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.0\Bin\JetBrains.Platform.dotCover.Shell.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.0\Bin\JetBrains.Platform.dotCover.UI.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.0\Bin\JetBrains.Platform.dotCover.Util.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.0\Bin\JetBrains.Platform.dotCover.VisualStudio.Core.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.0\Bin\JetBrains.dotCover.Psi.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.0\Bin\JetBrains.dotCover.Resources.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.0\Bin\JetBrains.dotCover.TaskRunnerFramework.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.0\Bin\JetBrains.dotCover.UnitTestExplorer.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.0\Bin\JetBrains.dotCover.UnitTestFramework.???" > nul
-cd ..
-echo Support for dotCover 2.0 successfully copied.
-
-:CopyDotCover_v21
-
-if not exist "%ProgFiles%\JetBrains\dotCover\v2.1\bin" goto CopyDotCover_v22
-
-mkdir dotCover_v2.1
-cd dotCover_v2.1
-
-copy "%ProgFiles%\JetBrains\dotCover\v2.1\Bin\JetBrains.Annotations.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.1\Bin\JetBrains.Platform.dotCover.ComponentModel.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.1\Bin\JetBrains.Platform.dotCover.DocumentModel.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.1\Bin\JetBrains.Platform.dotCover.IDE.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.1\Bin\JetBrains.Platform.dotCover.MetaData.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.1\Bin\JetBrains.Platform.dotCover.ProjectModel.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.1\Bin\JetBrains.Platform.dotCover.Shell.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.1\Bin\JetBrains.Platform.dotCover.UI.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.1\Bin\JetBrains.Platform.dotCover.Util.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.1\Bin\JetBrains.Platform.dotCover.VisualStudio.Core.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.1\Bin\JetBrains.dotCover.Psi.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.1\Bin\JetBrains.dotCover.Resources.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.1\Bin\JetBrains.dotCover.TaskRunnerFramework.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.1\Bin\JetBrains.dotCover.UnitTestExplorer.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.1\Bin\JetBrains.dotCover.UnitTestFramework.???" > nul
-cd ..
-echo Support for dotCover 2.1 successfully copied.
-
-:CopyDotCover_v22
-
-if not exist "%ProgFiles%\JetBrains\dotCover\v2.2\bin" goto CopyDotCover_v25
-
-mkdir dotCover_v2.2
-cd dotCover_v2.2
-
-copy "%ProgFiles%\JetBrains\dotCover\v2.2\Bin\JetBrains.Annotations.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.2\Bin\JetBrains.Platform.dotCover.ComponentModel.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.2\Bin\JetBrains.Platform.dotCover.DocumentModel.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.2\Bin\JetBrains.Platform.dotCover.IDE.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.2\Bin\JetBrains.Platform.dotCover.MetaData.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.2\Bin\JetBrains.Platform.dotCover.ProjectModel.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.2\Bin\JetBrains.Platform.dotCover.Shell.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.2\Bin\JetBrains.Platform.dotCover.UI.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.2\Bin\JetBrains.Platform.dotCover.Util.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.2\Bin\JetBrains.Platform.dotCover.VisualStudio.Core.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.2\Bin\JetBrains.dotCover.Features.Shared.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.2\Bin\JetBrains.dotCover.Psi.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.2\Bin\JetBrains.dotCover.Resources.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.2\Bin\JetBrains.dotCover.TaskRunnerFramework.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.2\Bin\JetBrains.dotCover.UnitTestExplorer.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.2\Bin\JetBrains.dotCover.UnitTestFramework.???" > nul
-cd ..
-echo Support for dotCover 2.2 successfully copied.
-
-:CopyDotCover_v25
-
-if not exist "%ProgFiles%\JetBrains\dotCover\v2.5\bin" goto CopyDotCover_v26
-
-mkdir dotCover_v2.5
-cd dotCover_v2.5
-
-copy "%ProgFiles%\JetBrains\dotCover\v2.5\Bin\JetBrains.Platform.dotCover.ComponentModel.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.5\Bin\JetBrains.Platform.dotCover.DocumentModel.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.5\Bin\JetBrains.Platform.dotCover.IDE.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.5\Bin\JetBrains.Platform.dotCover.Metadata.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.5\Bin\JetBrains.Platform.dotCover.ProjectModel.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.5\Bin\JetBrains.Platform.dotCover.Shell.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.5\Bin\JetBrains.Platform.dotCover.UI.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.5\Bin\JetBrains.Platform.dotCover.Util.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.5\Bin\JetBrains.Platform.dotCover.VisualStudio.Core.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.5\Bin\JetBrains.PsiFeatures.dotCover.Features.Shared.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.5\Bin\JetBrains.PsiFeatures.dotCover.Psi.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.5\Bin\JetBrains.PsiFeatures.dotCover.Resources.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.5\Bin\JetBrains.PsiFeatures.dotCover.TaskRunnerFramework.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.5\Bin\JetBrains.PsiFeatures.dotCover.UnitTestExplorer.???" > nul
-copy "%ProgFiles%\JetBrains\dotCover\v2.5\Bin\JetBrains.PsiFeatures.dotCover.UnitTestFramework.???" > nul
-cd ..
-echo Support for dotCover 2.5 successfully copied.
 
 :CopyDotCover_v26
 
--- resharper/src/ProductInfo.dotCover.2.0.cs
+++
@@ -1,6 +0,0 @@
-public static class ProductInfo
-{
-    public const string Product = "dotCover";
-    public const string AssemblyProduct = "dotcover";
-    public const string Version = "2.0.406";
-}

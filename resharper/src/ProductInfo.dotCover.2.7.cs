b698347158d4e3ae595a4a56c1ba43320ec5130d
citizenmatt <m.t.ellis@gmail.com>
2014-03-21 10:50:30 +0000

    dotCover 2.7 RTM product info version

--- resharper/src/ProductInfo.dotCover.2.7.cs
+++ resharper/src/ProductInfo.dotCover.2.7.cs
@@ -2,5 +2,5 @@ internal static class ProductInfo
 {
     public const string Product = "dotCover";
     public const string AssemblyProduct = "dotcover";
-    public const string Version = "2.7.0.0";
+    public const string Version = "2.7.0.662";
 }

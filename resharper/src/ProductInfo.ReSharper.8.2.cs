c9daffdabd246f2a37ae6c5ddcd43ed0b25b93b4
citizenmatt <m.t.ellis@gmail.com>
2014-03-21 10:50:21 +0000

    Updated to 8.2 RTM

--- resharper/src/ProductInfo.ReSharper.8.2.cs
+++ resharper/src/ProductInfo.ReSharper.8.2.cs
@@ -2,5 +2,5 @@ public static class ProductInfo
 {
     public const string Product = "ReSharper";
     public const string AssemblyProduct = "resharper";
-    public const string Version = "8.2.0.441-EAP";
+    public const string Version = "8.2.0.2160";
 }

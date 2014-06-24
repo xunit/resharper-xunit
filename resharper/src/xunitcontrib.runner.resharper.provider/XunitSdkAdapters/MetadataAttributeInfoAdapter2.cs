cd6fc7705c4125f7a8433396e518672f80da50d9
Matt Ellis <m.t.ellis@gmail.com>
2014-06-24 19:02:24 +0100

    xunit2 metadata IAttributeInfo implementation

---
+++ resharper/src/xunitcontrib.runner.resharper.provider/XunitSdkAdapters/MetadataAttributeInfoAdapter2.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Metadata.Reader.API;
+using Xunit.Abstractions;
+
+namespace XunitContrib.Runner.ReSharper.UnitTestProvider
+{
+    public class MetadataAttributeInfoAdapter2 : IAttributeInfo
+    {
+        private readonly IMetadataCustomAttribute attribute;
+
+        public MetadataAttributeInfoAdapter2(IMetadataCustomAttribute attribute)
+        {
+            this.attribute = attribute;
+        }
+
+        public IEnumerable<object> GetConstructorArguments()
+        {
+            return attribute.ConstructorArguments.Select(arg => arg.Value);
+        }
+
+        public IEnumerable<IAttributeInfo> GetCustomAttributes(string assemblyQualifiedAttributeTypeName)
+        {
+            var attributeType = attribute.UsedConstructor.DeclaringType;
+            foreach (var a in attributeType.CustomAttributes)
+            {
+                var type = a.UsedConstructor.DeclaringType;
+                while (type != null)
+                {
+                    if (type.AssemblyQualifiedName == assemblyQualifiedAttributeTypeName)
+                        yield return new MetadataAttributeInfoAdapter2(a);
+                    type = type.Base != null ? type.Base.Type : null;
+                }
+            }
+        }
+
+        public TValue GetNamedArgument<TValue>(string argumentName)
+        {
+            foreach (var initialization in attribute.InitializedProperties)
+            {
+                if (initialization.Property.Name == argumentName)
+                    return (TValue) initialization.Value.Value;
+            }
+            foreach (var initialization in attribute.InitializedFields)
+            {
+                if (initialization.Field.Name == argumentName)
+                    return (TValue) initialization.Value.Value;
+            }
+
+            return default(TValue);
+        }
+    }
+}
---
+++ resharper/test/src/tests/Abstractions/MetadataAttributeInfoAdapterTest.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Metadata.Reader.API;
+using JetBrains.Metadata.Utils;
+using JetBrains.Util;
+using NUnit.Framework;
+using Xunit.Abstractions;
+using XunitContrib.Runner.ReSharper.UnitTestProvider;
+
+namespace XunitContrib.Runner.ReSharper.Tests.Abstractions
+{
+    public class MetadataAttributeInfoAdapterTest
+    {
+        [Test]
+        public void Should_return_constructor_arguments()
+        {
+            var attributes = GetAttributes(typeof (HasCtorArgs)).ToList();
+
+            Assert.AreEqual(1, attributes.Count);
+            var attribute = attributes[0];
+
+            var args = attribute.GetConstructorArguments().ToList();
+            Assert.NotNull(args);
+            Assert.AreEqual(3, args.Count);
+            Assert.AreEqual("hello", args[0]);
+            Assert.AreEqual(42, args[1]);
+
+            // This fails if the metadata loader doesn't have the assembly referenced
+            // Don't know if it's going to cause issues
+            //Assert.AreEqual(typeof(string), args[2]);
+        }
+
+        [Test]
+        public void Should_return_named_arguments()
+        {
+            var attributes = GetAttributes(typeof (HasNamedArgs)).ToList();
+
+            Assert.AreEqual(1, attributes.Count);
+            var attribute = attributes[0];
+
+            Assert.AreEqual("hello", attribute.GetNamedArgument<string>("StringProperty"));
+            Assert.AreEqual("world", attribute.GetNamedArgument<string>("StringField"));
+            Assert.AreEqual(42, attribute.GetNamedArgument<int>("IntProperty"));
+            Assert.AreEqual(24, attribute.GetNamedArgument<int>("IntField"));
+            Assert.Null(attribute.GetNamedArgument<string>("UnsetStringProperty"));
+            Assert.AreEqual(0, attribute.GetNamedArgument<int>("UnsetIntProperty"));
+        }
+
+        [Test]
+        public void Should_return_custom_attributes()
+        {
+            var attributes = GetAttributes(typeof(HasBoring)).ToList();
+
+            Assert.AreEqual(1, attributes.Count);
+            var attribute = attributes[0];
+
+            var customAttributes = attribute.GetCustomAttributes(typeof(FooAttribute).AssemblyQualifiedName);
+            Assert.NotNull(customAttributes);
+            Assert.AreEqual(2, customAttributes.Count());
+        }
+
+        private IEnumerable<IAttributeInfo> GetAttributes(Type targetType)
+        {
+            using (var loader = new MetadataLoader())
+            {
+                var assembly = loader.LoadFrom(FileSystemPath.Parse(targetType.Assembly.Location),
+                    JetFunc<AssemblyNameInfo>.True);
+
+                var type = assembly.GetTypeInfoFromQualifiedName(targetType.FullName, false);
+                return type.CustomAttributes.Select(a => new MetadataAttributeInfoAdapter2(a));
+            }
+
+            //return CustomAttributeData.GetCustomAttributes(targetType).Select(Reflector.Wrap);
+        }
+    }
+
+    public class CtorArgsAttribute : Attribute
+    {
+        public CtorArgsAttribute(string s, int i, Type t)
+        {
+        }
+    }
+
+    [CtorArgs("hello", 42, typeof(string))]
+    public class HasCtorArgs
+    {
+    }
+
+    public class NamedArgsAttribute : Attribute
+    {
+        public string StringProperty { get; set; }
+        public int IntProperty { get; set; }
+
+        public string StringField;
+        public int IntField;
+
+        public string UnsetStringProperty { get; set; }
+        public string UnsetIntProperty { get; set; }
+    }
+
+    [NamedArgs(StringProperty = "hello", StringField = "world", IntProperty = 42, IntField = 24)]
+    public class HasNamedArgs
+    {
+    }
+
+    public class FooAttribute : Attribute
+    {
+    }
+
+    public class Foo2Attribute : FooAttribute
+    {
+    }
+
+    [FooAttribute]
+    [Foo2]
+    public class BoringAttribute : Attribute
+    {
+    }
+
+    [Boring]
+    public class HasBoring
+    {
+    }
+}

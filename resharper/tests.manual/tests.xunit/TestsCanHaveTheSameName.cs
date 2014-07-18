6ca65bbc6eb966320f35fe5fb62789497fd9cf2c
Matt Ellis <m.t.ellis@gmail.com>
2014-07-18 10:19:43 +0100

    Added positive test for duplicate test method names

---
+++ resharper/tests.manual/tests.xunit/TestsCanHaveTheSameName.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+
+namespace tests.xunit.passing
+{
+    namespace TestsWithSameName
+    {
+        // xunit2 allows tests to have the same name. v1 treats this
+        // as an ambiguous name exception and is a catastrophic error
+        public class TestsCanHaveSameMethodName
+        {
+            [Fact]
+            public void TheTest()
+            {
+                Assert.Equal(12, 23);
+            }
+
+            [Theory]
+            [InlineData(42)]
+            public void TheTest(int x)
+            {
+            }
+
+            [Theory]
+            [InlineData("Hello, world")]
+            public void TheTest(string x)
+            {
+            }
+
+            private void TheTest(string x, int y)
+            {
+            }
+        }
+    }
+}

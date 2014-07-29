51480b0177d59b64046adc424ecce2b9f71737ff
Matt Ellis <m.t.ellis@gmail.com>
2014-07-29 15:29:55 +0100

    Get rid of MetadataGenericArgumentTypeInfoAdapter

--- resharper/src/xunitcontrib.runner.resharper.provider/XunitSdkAdapters/MetadataMethodInfoAdapter2.cs
+++ resharper/src/xunitcontrib.runner.resharper.provider/XunitSdkAdapters/MetadataMethodInfoAdapter2.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Metadata.Reader.API;
-using JetBrains.Util;
 using Xunit.Abstractions;
 
 namespace XunitContrib.Runner.ReSharper.UnitTestProvider
@@ -42,7 +41,7 @@ namespace XunitContrib.Runner.ReSharper.UnitTestProvider
             // IMetadataGenericArgument doesn't really give us enough to build an ITypeInfo with,
             // so create a new type with some default values
             return from a in method.GenericArguments
-                select (ITypeInfo) new MetadataGenericArgumentTypeInfoAdapter(typeInfo.Assembly, a);
+                select (ITypeInfo) new GenericArgumentTypeInfoAdapter(typeInfo.Assembly, a.Name);
         }
 
         public IEnumerable<IParameterInfo> GetParameters()
@@ -78,46 +77,5 @@ namespace XunitContrib.Runner.ReSharper.UnitTestProvider
         }
 
         public ITypeInfo Type { get { return typeInfo; } }
-
-        private class MetadataGenericArgumentTypeInfoAdapter : ITypeInfo
-        {
-            private readonly IMetadataGenericArgument argument;
-
-            public MetadataGenericArgumentTypeInfoAdapter(IAssemblyInfo assembly, IMetadataGenericArgument argument)
-            {
-                Assembly = assembly;
-                this.argument = argument;
-            }
-
-            public IEnumerable<IAttributeInfo> GetCustomAttributes(string assemblyQualifiedAttributeTypeName)
-            {
-                return EmptyList<IAttributeInfo>.InstanceList;
-            }
-
-            public IEnumerable<ITypeInfo> GetGenericArguments()
-            {
-                return EmptyList<ITypeInfo>.InstanceList;
-            }
-
-            public IMethodInfo GetMethod(string methodName, bool includePrivateMethod)
-            {
-                return null;
-            }
-
-            public IEnumerable<IMethodInfo> GetMethods(bool includePrivateMethods)
-            {
-                return EmptyList<IMethodInfo>.InstanceList;
-            }
-
-            public IAssemblyInfo Assembly { get; private set; }
-            public ITypeInfo BaseType { get { return null; } }
-            public IEnumerable<ITypeInfo> Interfaces { get { return EmptyList<ITypeInfo>.InstanceList; } }
-            public bool IsAbstract { get { return true; } }
-            public bool IsGenericParameter { get { return true; } }
-            public bool IsGenericType { get { return false; } }
-            public bool IsSealed { get { return true; } }
-            public bool IsValueType { get { return false; } }
-            public string Name { get { return argument.Name; } }
-        }
     }
 }

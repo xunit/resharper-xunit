bf988fb8fc0ca65fec9ad778b85a5aea59fd3bb5
Matt Ellis <m.t.ellis@gmail.com>
2014-11-28 20:58:26 +0000

    Updated vendor
    Actually, not sure these attributes are still used...

--- resharper/src/xunitcontrib.runner.resharper.provider/Properties/AssemblyInfo.cs
+++ resharper/src/xunitcontrib.runner.resharper.provider/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using JetBrains.Application.PluginSupport;
 
 [assembly: PluginTitle("xUnit.net unit test provider")]
 [assembly: PluginDescription("Allows " + ProductInfo.Product + " to run xUnit.net tests. Compiled against " + ProductInfo.Product + " " + ProductInfo.Version)]
-[assembly: PluginVendor("xunitcontrib Team")]
+[assembly: PluginVendor("Matt Ellis")]
 

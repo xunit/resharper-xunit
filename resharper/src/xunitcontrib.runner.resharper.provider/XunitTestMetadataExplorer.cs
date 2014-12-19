e6521e14356d677e7e6fab1faaf89500f98906f7
Matt Ellis <m.t.ellis@gmail.com>
2014-12-19 12:03:05 +0000

    Updated to ReSharper 9.0 RTM

--- resharper/src/xunitcontrib.runner.resharper.provider/XunitTestMetadataExplorer.cs
+++ resharper/src/xunitcontrib.runner.resharper.provider/XunitTestMetadataExplorer.cs
@@ -18,10 +18,6 @@ namespace XunitContrib.Runner.ReSharper.UnitTestProvider
         {
             this.provider = provider;
             this.unitTestElementFactory = unitTestElementFactory;
-
-            // Hmm. Not sure I like this here - needs to be here so that ReSharper will load
-            // the runner assembly from the external process, so that assumes this was done
-            //assemblyLoader.RegisterAssembly(typeof(XunitTaskRunner).Assembly);
         }
 
         public void ExploreAssembly(IProject project, IMetadataAssembly assembly, IUnitTestElementsObserver observer)

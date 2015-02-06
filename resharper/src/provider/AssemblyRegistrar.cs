19a178925d8978d96b2cfb2f5fdf33b478ef4317
Matt Ellis <m.t.ellis@gmail.com>
2015-02-06 16:01:10 +0000

    Register runner assembly so remoting works
    I don't understand why I have to register the assembly in an AppDomain that's already got the assembly loaded, but it's needed for 8.2. 9.0 doesn't need it because the runner assembly is in the same folder as the provider, so it gets found easily

---
+++ resharper/src/provider/AssemblyRegistrar.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.ProjectModel;
+using JetBrains.ReSharper.UnitTestFramework;
+using XunitContrib.Runner.ReSharper.RemoteRunner;
+
+namespace XunitContrib.Runner.ReSharper.UnitTestProvider
+{
+    [SolutionComponent]
+    public class AssemblyRegistrar
+    {
+        public AssemblyRegistrar(UnitTestingAssemblyLoader loader)
+        {
+            loader.RegisterAssembly(typeof(XunitTaskRunner).Assembly);
+        }
+    }
+}

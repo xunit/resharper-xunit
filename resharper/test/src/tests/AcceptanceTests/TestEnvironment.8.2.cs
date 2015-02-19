80bbeb14d81d32150d686c2ffd2000a5b34a3e29
Matt Ellis <m.t.ellis@gmail.com>
2015-02-19 00:52:33 +0000

    Hack to run tests in ReSharper 8.2 on .net 4.6

--- resharper/test/src/tests/AcceptanceTests/TestEnvironment.8.2.cs
+++ resharper/test/src/tests/AcceptanceTests/TestEnvironment.8.2.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Reflection;
 using JetBrains.Application;
+using JetBrains.Build.AllAssemblies;
+using JetBrains.ReSharper;
 using JetBrains.Threading;
 using NUnit.Framework;
 using XunitContrib.Runner.ReSharper.RemoteRunner;
@@ -16,14 +18,46 @@ namespace XunitContrib.Runner.ReSharper.Tests.AcceptanceTests
     [SetUpFixture]
     public class TestEnvironmentAssembly : ReSharperTestEnvironmentAssembly
     {
-        /// <summary>
-        /// Gets the assemblies to load into test environment.
-        /// Should include all assemblies which contain components.
-        /// </summary>
+        // ReSharper 8.2 doesn't know anything about .net 4.6, and throws exceptions.
+        // We can teach it by overriding the default FrameworkLocationHelper class, but
+        // it must be overridden before the ShellComponents are composed, and the call
+        // to AssemblyManager is SetUp is too late for that. So, we add this assembly
+        // into the list of product assemblies that are used by default when composing
+        // the Shell container. Hacky, but at least it lets us run the tests.
+        public override IApplicationDescriptor CreateApplicationDescriptor()
+        {
+            return new CustomApplicationDescriptor();
+        }
+
+        private class CustomApplicationDescriptor : ReSharperApplicationDescriptor
+        {
+            private AllAssembliesXml allAssembliesXml;
+
+            public override AllAssembliesXml AllAssembliesXml
+            {
+                get
+                {
+                    if (allAssembliesXml != null)
+                        return allAssembliesXml;
+
+                    allAssembliesXml = base.AllAssembliesXml;
+                    var assemblies = new List<ProductAssemblyXml>(allAssembliesXml.Assemblies)
+                    {
+                        new ProductAssemblyXml
+                        {
+                            Configurations = "Common",
+                            Name = typeof (Net46CompatibleFrameworkLocationHelper).Assembly.GetName().Name
+                        }
+                    };
+                    allAssembliesXml.Assemblies = assemblies.ToArray();
+                    return allAssembliesXml;
+                }
+            }
+        }
+
         private static IEnumerable<Assembly> GetAssembliesToLoad()
         {
-            // Test assembly
-            yield return Assembly.GetExecutingAssembly();
+//            yield return Assembly.GetExecutingAssembly();
 
             yield return typeof(XunitTestProvider).Assembly;
             yield return typeof(XunitTaskRunner).Assembly;

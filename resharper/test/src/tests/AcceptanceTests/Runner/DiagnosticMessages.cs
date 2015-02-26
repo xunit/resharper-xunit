28874d233a3b67a343ce94c63f1ee5365d36c843
Matt Ellis <m.t.ellis@gmail.com>
2015-02-26 16:56:57 +0000

    Comment

--- resharper/test/src/tests/AcceptanceTests/Runner/DiagnosticMessages.cs
+++ resharper/test/src/tests/AcceptanceTests/Runner/DiagnosticMessages.cs
@@ -2,6 +2,8 @@ using NUnit.Framework;
 
 namespace XunitContrib.Runner.ReSharper.Tests.AcceptanceTests.Runner
 {
+    // NOTE: Not included in 8.2, as 8.2 doesn't have a nice way of displaying
+    // diagnostics and so throws up a dialog box (pun intended)
     [Category("xunit2")]
     public class DiagnosticMessages : XunitTaskRunnerTestBase
     {

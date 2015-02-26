7d4727e1952c6b39310259fa0823b3bda54c1f53
Matt Ellis <m.t.ellis@gmail.com>
2015-02-26 17:10:41 +0000

    Updated tests to RC3

--- resharper/src/runner/TestRunner.cs
+++ resharper/src/runner/TestRunner.cs
@@ -56,7 +56,7 @@ namespace XunitContrib.Runner.ReSharper.RemoteRunner
             }
             catch (Exception e)
             {
-                var message = e.Message + Environment.NewLine + Environment.NewLine + "(Note: xUnit.net ReSharper runner requires projects are built with xUnit.net 2.0.0-rc2-build2857)";
+                var message = e.Message + Environment.NewLine + Environment.NewLine + "(Note: xUnit.net ReSharper runner requires projects are built with xUnit.net 2.0.0-rc3-build2880)";
                 var description = "Exception: " + e.ToString();
                 if (diagnosticMessages != null && diagnosticMessages.HasMessages)
                 {

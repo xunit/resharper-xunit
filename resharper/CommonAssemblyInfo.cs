893d91ca52816eb1ea8f16c35a4825a51129dfaa
Matt Ellis <m.t.ellis@gmail.com>
2015-04-10 18:11:57 +0100

    Updated nuspec for ReSharper 9.0

--- resharper/CommonAssemblyInfo.cs
+++ resharper/CommonAssemblyInfo.cs
@@ -6,4 +6,4 @@ using System.Runtime.InteropServices;
 [assembly : AssemblyDescription("xUnit.net unit test provider for " + ProductInfo.Product)]
 [assembly : AssemblyCopyright("Copyright (C) Matt Ellis")]
 [assembly : ComVisible(false)]
-[assembly : AssemblyVersion("2.0.0.*")]
+[assembly : AssemblyVersion("2.0.6.*")]

327334be9f4cbd897e3006dd795489cfd746f9e9
Matt Ellis <m.t.ellis@gmail.com>
2015-08-05 10:18:32 +0100

    Added missing 9.1 SDK tests + updated to latest SDK

--- resharper/test/src/tests/AcceptanceTests/Metadata/XunitMetadataTest.9.1.cs
+++ resharper/test/src/tests/AcceptanceTests/Metadata/XunitMetadataTest.9.1.cs
@@ -14,22 +14,7 @@ namespace XunitContrib.Runner.ReSharper.Tests.AcceptanceTests.Metadata
             // in TestCases, we fallback to the non-extension friendly implementation that expects 
             // the source to be laid out as if we're building the product, not an extension, and it 
             // requires Product.Root. We'll infer it instead. 
-            var productHomeDir =
-                Environment.GetEnvironmentVariable(AllAssembliesLocator.ProductHomeDirEnvironmentVariableName);
-            if (string.IsNullOrEmpty(productHomeDir))
-            {
-                var assembly = GetType().Assembly;
-                var productBinariesDir = assembly.GetPath().Parent;
-                if (AllAssembliesLocator.TryGetProductHomeDirOnSources(productBinariesDir).IsNullOrEmpty())
-                {
-                    var markerPath = TestUtil.GetTestDataPathBase_Find_FromAttr(assembly) ??
-                                     TestUtil.DefaultTestDataPath;
-                    var directoryNameOfItemAbove = FileSystemUtil.GetDirectoryNameOfItemAbove(productBinariesDir,
-                        markerPath);
-                    Environment.SetEnvironmentVariable(AllAssembliesLocator.ProductHomeDirEnvironmentVariableName,
-                        directoryNameOfItemAbove.FullPath);
-                }
-            }
+            TestUtil.SetHomeDir(GetType().Assembly);
         }
     }
 }

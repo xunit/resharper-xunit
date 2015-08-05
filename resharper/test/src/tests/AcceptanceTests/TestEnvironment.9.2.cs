f124b6ca304054feccedc19396ab40ee7ff3ad24
Matt Ellis <m.t.ellis@gmail.com>
2015-08-05 11:31:57 +0100

    9.2 testing requires STA
    Not sure if this is a new requirement or oversight...

--- resharper/test/src/tests/AcceptanceTests/TestEnvironment.9.2.cs
+++ resharper/test/src/tests/AcceptanceTests/TestEnvironment.9.2.cs
@@ -4,6 +4,9 @@ using JetBrains.TestFramework;
 using JetBrains.TestFramework.Application.Zones;
 using NUnit.Framework;
 
+// Hmm. 9.2 seems to require STA - ShellLocks asserts if we're not. I don't know why, yet.
+[assembly: RequiresSTA]
+
 namespace XunitContrib.Runner.ReSharper.Tests.AcceptanceTests
 {
     [ZoneDefinition]

02adafa5cffb06e737209359d68b5cd98e28a6da
Matt Ellis <m.t.ellis@gmail.com>
2015-11-03 00:04:37 +0000

    Bumped version numbers and product info

--- resharper/src/ProductInfo.cs
+++ resharper/src/ProductInfo.cs
@@ -2,5 +2,5 @@ public static class ProductInfo
 {
     public const string Product = "ReSharper";
     public const string AssemblyProduct = "resharper";
-    public const string Version = "9.0.20141127";
+    public const string Version = "9.2";
 }
